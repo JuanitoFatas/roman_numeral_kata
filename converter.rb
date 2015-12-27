304c17f51b82d5ac9dc87ef094f95e9ced9bf8d6
JuanitoFatas <katehuang0320@gmail.com>
2015-12-27 22:51:44 +0800

    Init with Roman Numeral Kata setup
    bundle init
    gem "rspec"
    bundle
    rspec --init
    converter.rb & spec/converter_spec.rb

---
+++ converter.rb
@@ -0,0 +1,4 @@
+class Converter
+  def decimal_to_roman(decimal)
+  end
+end
---
+++ spec/converter_spec.rb
@@ -0,0 +1,15 @@
+require_relative "../converter"
+
+RSpec.describe Converter do
+  let(:converter) { Converter.new }
+
+  def decimal_to_roman(decimal)
+    converter.decimal_to_roman(decimal)
+  end
+
+  describe "#decimal_to_roman" do
+    it { expect(decimal_to_roman(1)).to eq "I"   }
+    it { expect(decimal_to_roman(2)).to eq "II"  }
+    it { expect(decimal_to_roman(3)).to eq "III" }
+  end
+end
